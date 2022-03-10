50fc4a25d47f09893efd25ae55dcb8e156416d83
Inbarasan <inbarasainba8@gmail.com>
2022-03-10 09:30:08 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Youtube-Thingspeak python analysis
+
+## Requirements:
++ Python (--version 3.x.x).
++ Requests - Python package (Run `pip install requests` or `pip3 install requests`).
+
+## How to ?:
++ Create a API key from Google cloud platfor, Under `APIs & Services` with `Youtube Data API` enabled.
++ Paste the video id (`the last 11 characters of video url`) in the ytVideoid field of `analysis.py` file.
++ Create your Thingspeak channel with `4` fields (viewsCount, likesCount, commentCount, viewsIncrement).
++ Paste your Google API, Thingspeak APIs and urls in the fields of `analysis.py` file.
++ Run `analysis.py ` file.
++ View video statistics in your Thingspeak channel.
---
+++ analysis.py
@@ -0,0 +1,53 @@
+import requests,json,time
+
+googleApikey = '' #generate-api-kry-from-google-cloud-platform
+
+thingspeakWriteKey = '' #write-api-key-of-thingspeak-channel
+
+thingspeakReadKey = '' #read-api-key-of-thingspeak-channel
+
+ytVideoid = ''  #id-of-youtube-video
+
+yturl = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id='+ytVideoid+'&t&key='+googleApikey
+
+channelId = ''#id-of-thingspeak-channel
+
+getUrl = 'https://api.thingspeak.com/channels/'+channelId+'/feeds.json?api_key='+thingspeakReadKey+'&results=1'
+
+updateTime = 60 #Seconds
+
+while True:   
+    
+    result = requests.get(yturl)
+
+    result = json.loads(result.text)
+
+    result = result['items'][0]['statistics']
+
+    preResult = requests.get(getUrl)
+
+    preResult = json.loads(preResult.text)
+
+    try:
+        viewIncrement = int(result['viewCount'])-int(preResult['feeds'][0]['field1'])
+    except IndexError as e:
+        viewIncrement = 0
+
+    postUrl = "https://api.thingspeak.com/update?"+thingspeakWriteKey+"&field1="+result['viewCount']+"&field2="+result['likeCount']+"&field3="+result['commentCount']
+    
+    print("Views Increment:",viewIncrement)
+
+
+    if(viewIncrement>0):
+        result['viewsIncrement'] = str(viewIncrement)
+        postUrl = postUrl + "&field4="+result['viewsIncrement']
+
+    print(result)
+
+    z=requests.get(postUrl)
+
+    print(z.status_code)
+
+    print(time.strftime("%H:%M",time.localtime()))
+
+    time.sleep(updateTime)
